7bbb70c5961fb70b2073e775d51f51853a7a4dbe
damunguia <damunguia95@gmail.com>
2024-08-26 01:07:19 -0400

    Eliminacion de cross

--- src/main/java/com/streamingUnir/Controller/BitacoraController.java
+++ src/main/java/com/streamingUnir/Controller/BitacoraController.java
@@ -3,6 +3,7 @@ package com.streamingUnir.Controller;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,11 +14,13 @@ import com.streamingUnir.service.BitacoraService;
 
 
 @RestController
+@CrossOrigin(origins = {}, allowedHeaders = {})
 public class BitacoraController {
 	
 	@Autowired
 	 private BitacoraService bitacoraService;
 	
+	@CrossOrigin(origins = {}, allowedHeaders = {})
 	@PostMapping("/bitacora")
 	Bitacora addBitacora(@RequestBody Bitacora bitacora) {
 	    return bitacoraService.save(bitacora);
--- src/main/java/com/streamingUnir/Controller/CategoriaController.java
+++ src/main/java/com/streamingUnir/Controller/CategoriaController.java
@@ -7,7 +7,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,17 +18,20 @@ import com.streamingUnir.service.CategoriaService;
 
 
 @RestController
+@CrossOrigin(origins = {}, allowedHeaders = {})
 public class CategoriaController {
 	
 	@Autowired
 	 private CategoriaService categoriaService;
 	 
+	@CrossOrigin(origins = {}, allowedHeaders = {})
 	@GetMapping("/categorias")
 	    private ResponseEntity<Optional<List<Categoria>>> getAllCategorias() {
 		Optional<List<Categoria>> categoria = categoriaService.getAllCategorias();
 	        return ResponseEntity.status(HttpStatus.OK).body(categoria);
 	    }
 	 
+	  @CrossOrigin(origins = {}, allowedHeaders = {})
 	  @GetMapping("/categoria/{id}")
 	    private ResponseEntity<CategoriaReponse> getCategorias(@PathVariable("id") int id) {
 		  CategoriaReponse categoria = categoriaService.getCategoriaById(id);
